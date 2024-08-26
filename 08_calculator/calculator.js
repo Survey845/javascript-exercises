27e40c510c84f81194db69a11ce46cb8b6c84c03
Survey845 <bhanjaimukund@gmail.com>
2024-08-26 08:38:39 +0530

    javascript-exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,54 @@
-const add = function() {
+const add = function(num1,num2) {
+	let add = num1+num2;
+	return add;
+};
+
+const subtract = function(num1,num2) {
+	let sub = num1-num2;
+	return sub;
+};
+
+const sum = function(numArray) {
+	let numArrayLength = numArray.length;
+	let sum = 0;
+	if(numArrayLength==0){
+		return sum;
+	} else{
+		for(i=0;i<numArrayLength;i++){
+			sum+=numArray[i];
+		}
+
+		return sum;
+	}
 	
 };
 
-const subtract = function() {
+const multiply = function(multiplyArray) {
+	let multiplyArrayLength = multiplyArray.length;
+	let multipleAnswer = 1;
+	for(i=0;i<multiplyArrayLength;i++){
+		multipleAnswer*=multiplyArray[i];
+	}
+	return multipleAnswer;
+};
+
+const power = function(baseNum,powerNum) {
+	let answerNum = baseNum**powerNum;
+	return answerNum;
 	
 };
 
-const sum = function() {
-	
-};
-
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(factorialNum) {
+	let factorialAns = 1;
+	if(factorialNum==0 || factorialNum==1 ){
+		return 1;
+	} else{
+		for(i=factorialNum;i>1;i--){
+			factorialAns*=i;
+			
+		}
+		return factorialAns;
+	}
 };
 
 // Do not edit below this line
