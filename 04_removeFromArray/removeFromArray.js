a64489890bf736122d6e36645fda4a1f4eadfd26
Survey845 <bhanjaimukund@gmail.com>
2024-08-26 08:36:40 +0530

    javascript-exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,21 @@
-const removeFromArray = function() {
+const removeFromArray = function(arrayMain, ...arguementToRemove) {
+    let arrayMainLength = arrayMain.length;
+    let arguementToRemoveLength = arguementToRemove.length;
+    let ansArray = [];
+    for(i = 0; i<arrayMainLength; i++){
+        let check = true;
+        for(j=0;j<arguementToRemoveLength;j++){
+            if(arrayMain[i]===arguementToRemove[j]){
+                check = false;
+                break;
+            }           
+        }
+        if(check == true){
+            ansArray.push(arrayMain[i]);
+        }
+        
+    }
+    return ansArray;
 };
 
 // Do not edit below this line
