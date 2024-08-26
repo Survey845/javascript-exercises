5fa3e75f06cf40dab9f47806454539cf72ba44b2
Survey845 <bhanjaimukund@gmail.com>
2024-08-26 08:35:25 +0530

    javascript-exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(word, multiplier) {
+    let returnString ='';
+    if(multiplier<0){
+        returnString = "ERROR";
 
+    } else{
+        for(i=0;i<multiplier;i++){
+            returnString+=word;
+        }
+    }
+    return returnString;
 };
 
 // Do not edit below this line
