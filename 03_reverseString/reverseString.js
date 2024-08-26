4bbf98cb52716ed38f9aca1d0ce694040ade9125
Survey845 <bhanjaimukund@gmail.com>
2024-08-26 08:35:58 +0530

    javascript-exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(stringToBeReversed) {
+    let lengthOfString = stringToBeReversed.length;
+    let stringWhichIsReversed = ""
+    for(i=0;i<lengthOfString;i++){
+        stringWhichIsReversed += stringToBeReversed[lengthOfString-i-1];
+    }
+    return stringWhichIsReversed;
 };
 
 // Do not edit below this line
