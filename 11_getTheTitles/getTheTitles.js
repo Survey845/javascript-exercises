54f9784c0a10a8691b05161809ba1db8c9b67504
Survey845 <bhanjaimukund@gmail.com>
2024-08-26 08:39:58 +0530

    javascript-exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,11 @@
-const getTheTitles = function() {
+const getTheTitles = function(booksArray) {
+    let solArray =[];
+    let booksArrayLength = booksArray.length;
+    for(i=0;i<booksArrayLength;i++){
+        solArray.push(booksArray[i]["title"]);
+    }
 
+    return solArray;
 };
 
 // Do not edit below this line
