f9a9ea2e431da0d62cf06476c8582c48cc155979
Survey845 <bhanjaimukund@gmail.com>
2024-08-26 08:39:05 +0530

    javascript-exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,26 @@
-const palindromes = function () {
+const palindromes = function (stringToCheck) {
+        stringToCheck = stringToCheck.toLowerCase();
+        let letterArray = [];
+        let reverseArray = [];
+        let stringToCheckLength = stringToCheck.length;
+        for(i=0;i<stringToCheckLength;i++){
+            if(stringToCheck[i].search(/^[a-z0-9]+$/i) != -1){
+                letterArray.push(stringToCheck[i])
+            }
+        }
+        let letterArrayLength = letterArray.length;
 
+        for(j=1;j<=letterArrayLength;j++){
+            reverseArray.push(letterArray[letterArrayLength-j])
+        }
+
+        if(reverseArray.toString()===letterArray.toString()){
+            return true;
+        } else{
+            return false;
+        }
+    
+        
 };
 
 // Do not edit below this line
