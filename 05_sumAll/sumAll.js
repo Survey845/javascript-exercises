74daa6828d4bfe557b5b23c611fb45e8d9fd8c4b
Survey845 <bhanjaimukund@gmail.com>
2024-08-26 08:37:06 +0530

    javascript-exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let numOne,numTwo;
+    let totalSum = 0;
 
+    if(num1<0 || num2<0 || num1%1!==0 || num2%1!==0 || typeof num1!==typeof 0 || typeof num2!==typeof 0){
+        return "ERROR";
+    }
+    
+
+    else if(num1<num2){
+        numOne = num1;
+        numTwo = num2;
+    }
+    else{
+        numOne = num2;
+        numTwo = num1;
+    }
+
+    for(i=numOne;i<=numTwo;i++){
+        totalSum+=i;
+    }
+    return totalSum;
 };
 
 // Do not edit below this line
