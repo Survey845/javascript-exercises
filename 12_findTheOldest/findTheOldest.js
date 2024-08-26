73bbcbe0d59911299420947d45af8294b5d1afd7
Survey845 <bhanjaimukund@gmail.com>
2024-08-26 08:40:26 +0530

    javascript-exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(oldestPersonsList) {
+    let oldestPersonIndex;
+    let maxAge = 0;
+    let currentDate = parseInt(new Date().getFullYear());
+    let oldestPersonsListLength = oldestPersonsList.length;
+    for(i=0;i<oldestPersonsListLength;i++){
+        if(typeof oldestPersonsList[i].yearOfDeath==="undefined" && currentDate-oldestPersonsList[i]["yearOfBirth"]>maxAge){
+            maxAge = currentDate-oldestPersonsList[i]["yearOfBirth"];
+            oldestPersonIndex = i;
+            
+        } else if(oldestPersonsList[i]["yearOfDeath"]-oldestPersonsList[i]["yearOfBirth"]>maxAge){
+            maxAge = oldestPersonsList[i]["yearOfDeath"]-oldestPersonsList[i]["yearOfBirth"];
+            oldestPersonIndex = i;
+        } 
+    }
+    let oldestPerson = oldestPersonsList[oldestPersonIndex];
+    return oldestPerson;
+    
 };
 
 // Do not edit below this line
