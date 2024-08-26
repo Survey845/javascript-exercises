e47fdbca82189345bbff20e1fd36572db1522c40
Survey845 <bhanjaimukund@gmail.com>
2024-08-26 08:38:12 +0530

    javascript-exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(numFah) {
+  solCel = (numFah-32)*5/9;
+  return parseFloat(solCel.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(numCel) {
+    solFah = (numCel*9/5)+32;
+    return parseFloat(solFah.toFixed(1));
 };
 
 // Do not edit below this line
