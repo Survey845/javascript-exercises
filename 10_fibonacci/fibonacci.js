7e8b9ffb494414ad02c40de39ee0f4a7c803d43a
Survey845 <bhanjaimukund@gmail.com>
2024-08-26 08:39:27 +0530

    javascript-exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,25 @@
-const fibonacci = function() {
+const fibonacci = function(fibonacciIndex) {
+    fibonacciIndex = parseInt(fibonacciIndex);
+    let finalSum = 1;
+    let middleMan = 1;
+    let emptyBox;
+
+    if(fibonacciIndex>=0){
+    if(fibonacciIndex==0 || fibonacciIndex==1){
+        return fibonacciIndex;
+    } else{
+        for(i=2;i<fibonacciIndex;i++){
+            emptyBox = finalSum;
+            finalSum+=middleMan;
+            middleMan = emptyBox;
+
+        }
+        return finalSum;
+        }
+    } else{
+        return "OOPS";
+    }
+
 
 };
 
