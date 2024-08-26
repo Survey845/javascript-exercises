86857fe0e2490ca970ee418f2b95dcbbd3f1e5e8
Survey845 <bhanjaimukund@gmail.com>
2024-08-26 08:37:41 +0530

    javascript-exercise 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,9 @@
-const leapYears = function() {
-
+const leapYears = function(yearToCheck) {
+    if((yearToCheck%100!=0 && yearToCheck%4==0) || (yearToCheck%400==0)){
+        return true;
+    } else{
+        return false;
+    }
 };
 
 // Do not edit below this line
